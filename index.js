aaf311b5bf069205cd4b8d2d4269ab8a20a28b75
Vinícius Ventura <58307012+viniciusventura29@users.noreply.github.com>
2023-10-05 16:51:23 +0000

    math operations working

--- index.js
+++ index.js
@@ -1,90 +1,47 @@
-// const rinhaExecutor = require("./rinhaExecutor");
-// rinhaExecutor();
+const { rinhaExecutor } = require("./rinhaExecutor");
+rinhaExecutor()
 
-let file = require("./source.rinha.json");
-var operationList = []
-
-function transformArrayToMathExpression(array) {
-  // Join the array elements into a single string
-  const mathExpression = array.join(' ');
-
-  // Define a regular expression to match supported operators (+, -, *, /, %)
-  const operatorRegex = /(\d+)\s*([+\-*\/%])\s*(\d+)/;
-
-  // Replace operator strings with real operators and evaluate the expression
-  const result = mathExpression.replace(operatorRegex, (match, operand1, operator, operand2) => {
-    switch (operator) {
-      case '+':
-        return parseFloat(operand1) + parseFloat(operand2);
-      case '-':
-        return parseFloat(operand1) - parseFloat(operand2);
-      case '*':
-        return parseFloat(operand1) * parseFloat(operand2);
-      case '/':
-        if (parseFloat(operand2) !== 0) {
-          return parseFloat(operand1) / parseFloat(operand2);
-        } else {
-          throw new Error('Division by zero is not allowed.');
-        }
-      case '%':
-        return parseFloat(operand1) % parseFloat(operand2);
-      default:
-        return match; // Return the original string if the operator is not supported
-    }
-  });
-
-  console.log(result)
-
-  return result;
+const operacoes = {
+  "Add": (left, right) => {
+    return operation(left) + operation(right)
+  },
+  "Sub": (left, right) => {
+    return operation(left) - operation(right)
+  },
+  "Mul":(left,right)=>{
+    return operation(left) * operation(right)
+  },
+  "Div":(left,right)=>{
+    return operation(left) / operation(right)
+  }
 }
 
-switch (file.expression.kind) {
-  case "Print":
-  var result = transformArrayToMathExpression(operation(file.expression.value, operationList))
-  console.log(result)
-}
 
-function operation(expression, operationList) {
+function operation(expression) {
+  if (expression.kind == "Print") {
+    const val = operation(expression.value);
+    return console.log(val)
+  }
   if (!expression.op) {
-    return expression.value.value
-  }
-  switch (expression.op) {
-    case "Add":
-      return Add(expression, operationList);
-    case "Sub":
-      return Sub(expression, operationList);
+      return expression.value
   }
 
+  return operacoes[expression.op](expression.lhs, expression.rhs)
 }
+  
+let file = require("./source.rinha.json");
+operation(file.expression);
 
-function Add(value, operationList) {
-  if (value.lhs.kind == "Str" || value.rhs.kind == "Str") {
-    if (value.rhs.rhs) {
-      operationList.push(value.lhs.value.toString(), '+')
-      return operation(value.rhs, operationList);
-    }
-    operationList.push(value.lhs.value.toString(), '+', value.rhs.value.toString())
-    return operationList;
-  } else {
-    if (value.rhs.rhs) {
-      operationList.push(value.lhs.value, '+')
-      return operation(value.rhs, operationList);
-    }
-    operationList.push(value.rhs.value + value.lhs.value)
-    return operationList;
-  }
-}
-
-function Sub(value, operationList) {
-  if (value.lhs.kind == "Str" || value.rhs.kind == "Str") {
-    throw new Error("You can not subtract a string!");
-  } else {
-    if (value.rhs.rhs) {
-      operationList.push(value.lhs.value,'-')
-      return Sub(value.rhs, operationList);
-    } else {
-      operationList.push(value.lhs.value,'-', value.rhs.value)
-      return operationList;
-    }
-  }
-}
+// function Sub(value, operationList) {
+//   if (value.lhs.kind == "Str" || value.rhs.kind == "Str") {
+//     throw new Error("You can not subtract a string!");
+//   } else {
+//     if (value.rhs.rhs) {
+//       operationList.push(value.lhs.value,'-')
+//       return Sub(value.rhs, operationList);
+//     } else {
+//       operationList.push(value.lhs.value,'-', value.rhs.value)
+//       return operationList;
+//     }
+//   }
+// }
--- rinhaExecutor.js
+++ rinhaExecutor.js
@@ -1,16 +1,19 @@
 var exec = require("child_process").exec;
 var fs = require("fs");
 
-module.exports = function rinhaExecutor() {
-  exec("rinha .\\source.rinha", function (error, stdout, stderr) {
-    fs.writeFile("source.rinha.json", stdout, function (err) {
-      if (err) {
-        console.log(err);
+module.exports = {
+  rinhaExecutor: function () {
+    exec("rinha ./source.rinha", function (error, stdout, stderr) {
+      console.log('std,', stdout)
+      fs.writeFile("./source.rinha.json", stdout, function (err) {
+        if (err) {
+          console.log(err);
+        }
+      });
+      console.log(stderr);
+      if (error !== null) {
+        console.log("exec error: " + error);
       }
     });
-    console.log(stderr);
-    if (error !== null) {
-      console.log("exec error: " + error);
-    }
-  });
+  }
 }
